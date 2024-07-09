c6fca50e9ec027574f80f689070e36222059851b
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-07-09 12:06:43 +0200

    azadì in progress

--- src/components/Azadì.jsx
+++ src/components/Azadì.jsx
@@ -1,42 +1,31 @@
 import { useState } from "react";
-import "../assets/css/components.css";
+import "../assets/css/azadì.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import AzadìBackgroundDesktop from "../assets/img/azadì-background-desktop.png";
 import AzadìImage from "../assets/img/azadì-image.png";
 
 function Azadì() {
   return (
     <>
       <section className="azadì">
-        <img
-          src={AzadìBackgroundDesktop}
-          className="img-fluid background-image-desktop"
-          alt="Paesaggio Montagna"
-        />
-        <div className="container-fluid d-flex main-container-2">
-          <div className="container mt-5">
-            <div className="row d-flex align-items-center justify-content-center">
-              <div className="col-lg-6 col-12 p-4 d-flex justify-content-center align-items-center">
-                <img
-                  src={AzadìImage}
-                  alt="Home"
-                  className="img-fluid rounded-circle inside-image"
-                />
-              </div>
-              <div className="col-lg-6 col-12 p-4 d-flex flex-column justify-content-center align-items-center bg-white p-4 container-text">
-                <p className="container-title">Azadi!</p>
-                <p className="container-description">
-                  Descrizione breve libro Descrizione breve libro Descrizione
-                  breve libro Descrizione breve libro Descrizione breve libro
-                  Descrizione breve libro Descrizione breve libro Descrizione
-                  breve libro Descrizione breve libro Descrizione breve libro
-                  Descrizione breve libro Descrizione breve libro Descrizione
-                  breve libro Descrizione breve libro
-                </p>
-                <button type="button" className="btn btn-primary single-page-btn">
-                  ACQUISTA
-                </button>
-              </div>
+        <div className="container-fluid d-flex align-items-center azadì-container">
+          <div className="row azadì-row">
+            <div className="col-lg-4 col-12">
+              <img
+                src={AzadìImage}
+                alt="Home"
+                className="img-fluid rounded-circle azadì-image"
+              />
+            </div>
+            <div className="col-lg-4 col-12 d-flex flex-column align-items-center justify-content-center">
+              <div className="azadì-title">AZADI'</div>
+              <div className="azadì-text">Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro Descrizione approfondita libro </div>
+            </div>
+            <div className="col-lg-4 col-12">
+              <img
+                src={AzadìImage}
+                alt="Home"
+                className="img-fluid rounded-circle azadì-image"
+              />
             </div>
           </div>
         </div>
