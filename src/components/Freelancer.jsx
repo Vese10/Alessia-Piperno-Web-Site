4f2d1e2975c0b390be96de1693a1670e493a54a1
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-07-11 01:40:20 +0200

    freelancer page ok

--- src/components/Freelancer.jsx
+++ src/components/Freelancer.jsx
@@ -1,36 +1,51 @@
 import { useState } from "react";
 import "../assets/css/components.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import FreelancerBackgroundDesktop from "../assets/img/freelancer-background-desktop.png";
+import FreelancerTopPc from "../assets/img/freelancer-top-pc.png";
+import FreelancerTopMobile from "../assets/img/freelancer-top-mobile.png";
 
 function Freelancer() {
   return (
     <>
       <section className="freelancer">
-        <img
-          src={FreelancerBackgroundDesktop}
-          className="img-fluid background-image-desktop"
-          alt="Paesaggio Montagna"
-        />
-        <div className="container-fluid d-flex main-container-1">
-          <div className="container mt-5">
-            <div className="row d-flex align-items-center justify-content-center">
-              <div className="col-lg-6 col-12 p-4 d-flex flex-column justify-content-center align-items-center bg-white p-4 container-text">
-                <p className="container-title">FREELANCER</p>
-                <p className="container-description">
-                  Presentazione attività Presentazione attività Presentazione
-                  attività Presentazione attività Presentazione attività
-                  Presentazione attività Presentazione attività Presentazione
-                  attività Presentazione attività Presentazione attività
-                  Presentazione attività Presentazione attività Presentazione
-                  attività Presentazione attività Presentazione attività
-                  Presentazione attività
-                </p>
-                <button type="button" className="btn btn-primary single-page-btn">
-                  APPROFONDISCI
-                </button>
-              </div>
-              <div className="col-lg-4"></div>
+        <div className="container-fluid d-flex flex-column align-intems-center justify-content-center p-0">
+          <div className="d-flex flex-column align-items-center justify-content-center">
+            <div className="d-flex text-center align-items-end freelancer-top">
+              <p className="container-title freelancer-title-top p-4 m-0">
+                La mia attività da freelancer
+              </p>
+              <img
+                src={FreelancerTopPc}
+                className="img-fluid freelancer-image-pc"
+                alt="Paesaggio montano"
+              ></img>
+              <img
+                src={FreelancerTopMobile}
+                className="img-fluid freelancer-image-mobile"
+                alt="Paesaggio montano"
+              ></img>
+            </div>
+            <div className="container-fluid d-flex flex-column justify-content-center p-4">
+              <p className="container-title freelancer-title">Titolo 1</p>
+              <p className="container-description freelancer-description">
+                Approfondimento attività da freelancer Approfondimento attività
+                da freelancer Approfondimento attività da freelancer
+                Approfondimento attività da freelancer Approfondimento attività
+                da freelancer Approfondimento attività da freelancer
+                Approfondimento attività da freelancer Approfondimento attività
+                da freelancer Approfondimento attività da freelancer
+              </p>
+            </div>
+            <div className="container-fluid d-flex flex-column justify-content-center p-4">
+              <p className="container-title freelancer-title">Titolo 2</p>
+              <p className="container-description freelancer-description">
+                Approfondimento attività da freelancer Approfondimento attività
+                da freelancer Approfondimento attività da freelancer
+                Approfondimento attività da freelancer Approfondimento attività
+                da freelancer Approfondimento attività da freelancer
+                Approfondimento attività da freelancer Approfondimento attività
+                da freelancer Approfondimento attività da freelancer
+              </p>
             </div>
           </div>
         </div>
