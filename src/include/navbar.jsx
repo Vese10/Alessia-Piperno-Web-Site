de41e640991c544e80875b770779b2024e62ebdf
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-07-12 12:12:40 +0200

    lang progress

--- src/include/navbar.jsx
+++ src/include/navbar.jsx
@@ -17,12 +17,12 @@ import deFlag from "../assets/img/de.png";
 import ptFlag from "../assets/img/pt.png";
 
 const languages = [
-  { code: 'it', name: 'Italiano', flag: itFlag },
-  { code: 'en', name: 'English', flag: ukFlag },
-  { code: 'es', name: 'Español', flag: esFlag },
-  { code: 'fr', name: 'Français', flag: frFlag },
-  { code: 'de', name: 'Deutsch', flag: deFlag },
-  { code: 'pt', name: 'Português', flag: ptFlag },
+  { code: "it", name: "Italiano", flag: itFlag },
+  { code: "en", name: "English", flag: ukFlag },
+  { code: "es", name: "Español", flag: esFlag },
+  { code: "fr", name: "Français", flag: frFlag },
+  { code: "de", name: "Deutsch", flag: deFlag },
+  { code: "pt", name: "Português", flag: ptFlag },
 ];
 
 function Navbar({ setCurrentPage, setLanguage }) {
@@ -37,12 +37,12 @@ function Navbar({ setCurrentPage, setLanguage }) {
   const handleNavClick = (page) => {
     setCurrentPage(page);
     closeMenu();
-  }
+  };
 
   const handleLanguageChange = (language) => {
     setLanguage(language);
     closeLanguageMenu();
-  }
+  };
 
   return (
     <>
@@ -57,43 +57,91 @@ function Navbar({ setCurrentPage, setLanguage }) {
               aria-label="Toggle navigation"
               onClick={toggleMenu}
             >
-              <span className={`navbar-toggler-icon ${menuOpen ? 'open' : ''}`}></span>
+              <span
+                className={`navbar-toggler-icon ${menuOpen ? "open" : ""}`}
+              ></span>
             </button>
-            <a className="btn btn-custom d-lg-none d-flex flex-column" onClick={() => handleNavClick('home')}>
-              <span className="travel-adventure">TRAVEL.</span><span className="travel-adventure">ADVENTURE.</span>FREEDOM
+            <a
+              className="btn btn-custom d-lg-none d-flex flex-column"
+              onClick={() => handleNavClick("home")}
+            >
+              <span className="travel-adventure">TRAVEL.</span>
+              <span className="travel-adventure">ADVENTURE.</span>FREEDOM
             </a>
-            <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarNav">
+            <div
+              className="central-item"
+              onClick={toggleLanguageMenu}
+            >
+              <img src={Lingua} className="nav-img-lang"></img>
+              {languageMenuOpen && (
+                <div className="dropdown-menu show">
+                  {languages.map((lang) => (
+                    <a
+                      key={lang.code}
+                      className="dropdown-item"
+                      onClick={() => handleLanguageChange(lang.code)}
+                    >
+                      <img
+                        src={lang.flag}
+                        className="flag-img"
+                        alt={`${lang.name} flag`}
+                      />{" "}
+                      {lang.name}
+                    </a>
+                  ))}
+                </div>
+              )}
+            </div>
+            <div
+              className={`collapse navbar-collapse ${menuOpen ? "show" : ""}`}
+              id="navbarNav"
+            >
               <div className="navbar-nav mr-auto w-100">
-                <a className="nav-item" onClick={() => handleNavClick('home')}>
+                <a className="nav-item" onClick={() => handleNavClick("home")}>
                   <img src={Home} className="nav-img"></img>
                   <p className="nav-link">Home</p>
                 </a>
-                <a className="nav-item" onClick={() => handleNavClick('azadì')}>
+                <a className="nav-item" onClick={() => handleNavClick("azadì")}>
                   <img src={Azadì} className="nav-img"></img>
                   <p className="nav-link">Azadì!</p>
                 </a>
-                <a className="nav-item" onClick={() => handleNavClick('freelancer')}>
+                <a
+                  className="nav-item"
+                  onClick={() => handleNavClick("freelancer")}
+                >
                   <img src={Freelancer} className="nav-img"></img>
                   <p className="nav-link">Freelancer</p>
                 </a>
-                <a className="nav-item" onClick={() => handleNavClick('viaggi')}>
+                <a
+                  className="nav-item"
+                  onClick={() => handleNavClick("viaggi")}
+                >
                   <img src={Viaggi} className="nav-img"></img>
                   <p className="nav-link">Viaggi</p>
                 </a>
-                <a className="nav-item" onClick={() => handleNavClick('contatti')}>
+                <a
+                  className="nav-item"
+                  onClick={() => handleNavClick("contatti")}
+                >
                   <img src={Contatti} className="nav-img"></img>
                   <p className="nav-link">Contatti</p>
                 </a>
                 <div className="logo d-none d-lg-flex justify-content-center align-items-center w-100">
-                  <a className="nav-link travel-adventure" onClick={() => handleNavClick('home')}>
+                  <a
+                    className="nav-link travel-adventure"
+                    onClick={() => handleNavClick("home")}
+                  >
                     TRAVEL.ADVENTURE.<span className="freedom">FREEDOM</span>
                   </a>
                 </div>
-                <a className="nav-item" onClick={() => handleNavClick('login')}>
+                <a className="nav-item" onClick={() => handleNavClick("login")}>
                   <img src={Login} className="nav-img"></img>
                   <p className="nav-link">Login</p>
                 </a>
-                <div className="nav-item dropdown" onClick={toggleLanguageMenu}>
+                <div
+                  className="nav-item dropdown nav-lang"
+                  onClick={toggleLanguageMenu}
+                >
                   <img src={Lingua} className="nav-img"></img>
                   <p className="nav-link">Lingua</p>
                   {languageMenuOpen && (
@@ -104,7 +152,12 @@ function Navbar({ setCurrentPage, setLanguage }) {
                           className="dropdown-item"
                           onClick={() => handleLanguageChange(lang.code)}
                         >
-                          <img src={lang.flag} className="flag-img" alt={`${lang.name} flag`} /> {lang.name}
+                          <img
+                            src={lang.flag}
+                            className="flag-img"
+                            alt={`${lang.name} flag`}
+                          />{" "}
+                          {lang.name}
                         </a>
                       ))}
                     </div>
