906ced9f25c53f739c8d7f333690f5ebc8465680
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-08-13 18:42:48 +0200

    add trip try

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -7,7 +7,7 @@ const admin = require('../middlewares/admin');
 const router = express.Router();
 
 router.post('/trips', auth, admin, addTrip); // Solo amministratori possono creare trip
-router.get('/trips', auth, getTrips); // Tutti gli utenti possono visualizzare i trip
+router.get('/trips', getTrips); // Tutti gli utenti possono visualizzare i trip
 router.post('/trips/:id/join', auth, joinTrip); // Utenti possono iscriversi ai trip
 
 module.exports = router;
