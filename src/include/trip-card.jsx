b635c52590a83c0eb1ba28b0bbcf3eea09890077
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-08-14 15:48:04 +0200

    fixing errors

--- src/include/trip-card.jsx
+++ src/include/trip-card.jsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 import "../assets/css/components.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function TripCard({ trip, setCurrentPage }) {
+function TripCard({ trip, setCurrentPage, onDelete }) {
   const { t } = useTranslation();
   const isLoggedIn = !!sessionStorage.getItem("token");
   const role = sessionStorage.getItem("role");
