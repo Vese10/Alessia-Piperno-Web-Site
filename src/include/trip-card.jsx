06478a9ed84648992f7fa75bf3c8a43644c54a18
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-08-20 11:35:28 +0200

    edit trippp

--- src/include/trip-card.jsx
+++ src/include/trip-card.jsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 import "../assets/css/components.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function TripCard({ trip, setCurrentPage, onDelete, onEdit }) {
+function TripCard({ trip, onDelete, onEdit, setCurrentPage }) {
   const { t } = useTranslation();
   const isLoggedIn = !!sessionStorage.getItem("token");
   const role = sessionStorage.getItem("role");
