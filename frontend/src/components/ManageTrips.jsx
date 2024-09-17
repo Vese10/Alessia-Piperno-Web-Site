8873d8b74652a195b2ce4680ccd36a1bddbbc9b5
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-09-17 11:09:40 +0200

    Helmet OK

--- frontend/src/components/ManageTrips.jsx
+++ frontend/src/components/ManageTrips.jsx
@@ -1,6 +1,7 @@
 // MyTrips.jsx
 import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet";
 import "../assets/css/components.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -35,52 +36,57 @@ function ManageTrips() {
   }, [t]);
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-8 offset-md-2">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title"> {t("usertrips.trips")}</h5>
-              {errorMessage && <p className="text-danger">{errorMessage}</p>}
-              <ul className="list-group">
-                {trips.length > 0 ? (
-                  trips.map((trip) => (
-                    <li key={trip._id} className="list-group-item">
-                      <h5>{trip.nation}</h5>
-                      <p>{trip.description}</p>
-                      <p>
-                        {t("usertrips.date")}:{" "}
-                        {new Date(trip.date).toLocaleDateString()}
-                      </p>
-                      <p>
-                        {t("usertrips.duration")}: {trip.duration}
-                      </p>
-                      <p>
-                        {t("usertrips.price")}: {trip.price}€
-                      </p>
-                      <p>
-                        {t("usertrips.participants")}:{" "}
-                        {trip.participants.length}/{trip.maxParticipants}
-                      </p>
-                      <h6>{t("usertrips.participantsList")}:</h6>
-                      <ul>
-                        {trip.participants.map((user) => (
-                          <li key={user._id}>
-                            {user.name} {user.surname}
-                          </li>
-                        ))}
-                      </ul>
-                    </li>
-                  ))
-                ) : (
-                  <p>{t("usertrips.noTrips")}</p>
-                )}
-              </ul>
+    <>
+      <Helmet>
+        <title>Alessia Piperno - MyTrips</title>
+      </Helmet>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title"> {t("usertrips.trips")}</h5>
+                {errorMessage && <p className="text-danger">{errorMessage}</p>}
+                <ul className="list-group">
+                  {trips.length > 0 ? (
+                    trips.map((trip) => (
+                      <li key={trip._id} className="list-group-item">
+                        <h5>{trip.nation}</h5>
+                        <p>{trip.description}</p>
+                        <p>
+                          {t("usertrips.date")}:{" "}
+                          {new Date(trip.date).toLocaleDateString()}
+                        </p>
+                        <p>
+                          {t("usertrips.duration")}: {trip.duration}
+                        </p>
+                        <p>
+                          {t("usertrips.price")}: {trip.price}€
+                        </p>
+                        <p>
+                          {t("usertrips.participants")}:{" "}
+                          {trip.participants.length}/{trip.maxParticipants}
+                        </p>
+                        <h6>{t("usertrips.participantsList")}:</h6>
+                        <ul>
+                          {trip.participants.map((user) => (
+                            <li key={user._id}>
+                              {user.name} {user.surname}
+                            </li>
+                          ))}
+                        </ul>
+                      </li>
+                    ))
+                  ) : (
+                    <p>{t("usertrips.noTrips")}</p>
+                  )}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
